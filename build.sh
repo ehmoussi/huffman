849ca7275269a5ce98d56a19cda983f063e70107
El Hadi Moussi <50993404+ehmoussi@users.noreply.github.com>
2025-04-01 12:46:31 +0200

    Improve build script
    - Separate build from prod
    - Add some flags

--- build.sh
+++ build.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-set -xe
+set -e
 
+if [ "$1" == "debug" ]; then
+    echo "Build in DEBUG mode..."
+    gcc -g -Wall -Wextra -Werror -pedantic -std=c99 -fstack-protector-all -fsanitize=address -fsanitize=undefined huffman.c -o huffman
+else
+    echo "Build in PRODUCTION mode..."
+    gcc -g -Wall -Wextra -Werror -pedantic -std=c99 -O2 huffman.c -o huffman
+fi
 
-gcc -Wall -Werror huffman.c -o huffman && \
 ./huffman
-
