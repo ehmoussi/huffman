3eaeb44ef60cc279d71c05d8373006be21d461f1
El Hadi Moussi <50993404+ehmoussi@users.noreply.github.com>
2025-04-01 12:36:07 +0200

    Add documentation and use of int instead of the char for find_minimum_char

--- huffman.c
+++ huffman.c
@@ -5,6 +5,9 @@
 
 #define MAX_CHAR 256
 
+/// @brief Count the frequency of each ASCII character for the given message
+/// @param message
+/// @param frequencies Array of frequencies for MAX_CHAR ASCII characters
 void count_frequencies(const char *message, size_t *frequencies)
 {
     if (message != NULL)
@@ -18,31 +21,47 @@ void count_frequencies(const char *message, size_t *frequencies)
     }
 }
 
+/// @brief Print the character and its frequency. If the character is not printable
+////       then its hexadecimal representation is used and specified.
+/// @param c character to print
+/// @param frequency frequency of the character
+void print_char_frequency(unsigned char c, size_t frequency)
+{
+    if (isprint(c))
+        printf("%c : %" PRIuPTR "\n", c, frequency);
+    else
+        printf("Hex: %x : %" PRIuPTR "\n", c, frequency);
+}
+
+/// @brief Print all the characters and its frequency when > 0 if the character
+////       is not printable the hexadecimal is print instead and specified
+/// @param frequencies Array of frequencies for MAX_CHAR ASCII characters
 void print_frequencies(const size_t *frequencies)
 {
-    for (int i = 0; i < MAX_CHAR; i++)
+    for (size_t i = 0; i < MAX_CHAR; i++)
     {
         if (frequencies[i] > 0)
         {
-            char c = (char)i;
-            if (isprint(c))
-                printf("%c : %" PRIuPTR "\n", c, frequencies[i]);
-            else
-                printf("Hex: %x : %" PRIuPTR "\n", c, frequencies[i]);
+            unsigned char c = (unsigned char)i;
+            print_char_frequency(c, frequencies[i]);
         }
     }
 }
 
-char find_minimum_char(const size_t *frequencies, size_t min_freq)
+/// @brief Find the character with the minimum frequency and a certain threshold
+/// @param frequencies Array of frequencies for MAX_CHAR ASCII characters
+/// @param threshold threshold above which which the search is done
+/// @return the minimum ASCII character or -1 if none was found
+int find_minimum_char(const size_t *frequencies, size_t threshold)
 {
-    char min_char = (char)-1;
+    int min_char = -1;
     size_t current_min = SIZE_MAX;
     for (size_t i = 0; i < MAX_CHAR; i++)
     {
-        if (frequencies[i] > min_freq && frequencies[i] < current_min)
+        if (frequencies[i] > threshold && frequencies[i] < current_min)
         {
             current_min = frequencies[i];
-            min_char = (char)i;
+            min_char = (int)i;
         }
     }
     return min_char;
@@ -51,13 +70,12 @@ char find_minimum_char(const size_t *frequencies, size_t min_freq)
 int main(void)
 {
     size_t frequencies[MAX_CHAR] = {0};
-    const char *message = "aabbccddbbeaebdddfffdbffddabbbbbcdefaabbcccccaabbddfffdcecc";
+    const char *message = "aabbccddbbeaebdddfffdbffddabbbbbcdefaabbcccccaabbddfffdcecc\t";
     // Count frequencies
     count_frequencies(message, frequencies);
     print_frequencies(frequencies);
     // Find the character with the minimum frequency greater than a given frequency
-    char min_char = find_minimum_char(frequencies, 0);
-    printf("min: %c\n", min_char);
-
+    int min_char = find_minimum_char(frequencies, 0);
+    print_char_frequency((unsigned char)min_char, frequencies[(size_t)min_char]);
     return 0;
 }
